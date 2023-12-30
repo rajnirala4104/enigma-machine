2308983b07902d1256724860ed532fc8ca543b66
rynsf <rynsf@users.noreply.github.com>
2023-12-30 21:37:48 +0530

    Add Plugboard

--- enigma.py
+++ enigma.py
@@ -1,11 +1,13 @@
 import pygame
 
 class Rotor():
-    def __init__(self, r, left=None, right=None):
+    def __init__(self, r, left=None, right=None, pos_rect):
         self.r = r
         self.pos = 0
         self.left = left
         self.right = right
+        self.pos_rect = pos_rect
+        self.set_button_up = Rect(pos_rect.x, pos_rect.y - pos_rect.h - 10, pos_rect.w, pos_rect.h)
 
     def output(self, s):
         if self.left == None:
@@ -29,6 +31,10 @@ class Rotor():
                 self.left.inc()
             self.pos = 0
 
+    def set_rotor(self, click_pos):
+        if self.set_button_up.collidepoint(click_pos):
+
+
     def render(self, x, y):
         surface = fontRotor.render(f" {self.pos:02d} ", True, "black", "grey")
         rect = screen.blit(surface, (x, y))
@@ -50,39 +56,141 @@ class KeyLight():
         surface = font.render(self.letter, True, colorFont)
         screen.blit(surface, (self.x - (size[0]/2), self.y - (size[1]/2)))
 
+class Socket():
+    def __init__(self, x, y, s):
+        self.x = x
+        self.y = y
+        self.s = s
 
-keys = {}
-keyPressed = None
-keyOut = None
-rotor = []
+    def render(self):
+        pygame.draw.circle(screen, "purple", (self.x, self.y), 20)
+        pygame.draw.circle(screen, "grey", (self.x, self.y), 15)
+        size = font.size(self.s)
+        surface = font.render(self.s, True, "black")
+        screen.blit(surface, (self.x - (size[0]/2), self.y - (size[1]/2) - 40))
+
+
+class PlugHead():
+    def __init__(self, x, y, w, s):
+        self.rect = pygame.Rect(x - w/2, y - w/2, w, w)
+        self.picked = False
+        self.s = s
+
+    def update(self, pos):
+        self.rect.x = pos[0] - self.ofset[0]
+        self.rect.y = pos[1] - self.ofset[1]
+
+    def pick(self, pos):
+        self.ofset = [pos[0] - self.rect.x, pos[1] - self.rect.y]
+        self.picked = True
+        global picked #TODO Better return than global
+        picked = True
+
+    def setPlug(self):
+        global picked
+        for s in sockets:
+            for p in plugs:
+                if s in (p.head[0].s, p.head[1].s):
+                    break
+            else:
+                if self.rect.colliderect((sockets[s].x - 20, sockets[s].y - 20, 40, 40)): #Remove magic numbers
+                    self.rect.x = sockets[s].x - 20
+                    self.rect.y = sockets[s].y - 20 #TODO remove magic number
+                    #print(rotor[0].r[alpha2index(self.s)], "to", rotor[0].r[alpha2index(s)])
+                    rotor[0].r[alpha2index(s)] = rotor[0].r[alpha2index(self.s)] #this is so inelegant. must have made a seperate class for the plugboard, it is so different form rotors. It behaves differently. Will do it in the next version of the code lets keep going for now.
+                    rotor[0].r[alpha2index(rotor[0].r[alpha2index(self.s)])] = s
+                    rotor[0].r[alpha2index(self.s)] = self.s
+                    self.s = s
+                    picked = False #TODO better return than global
+                    self.picked = False
+                    print(rotor[0].r)
+                    break
+        else:
+            self.rect.x = sockets[self.s].x - 20
+            self.rect.y = sockets[self.s].y - 20 #TODO remove magic number
+            self.picked = False
+            picked = False
+
+
+class Plug():
+    def __init__(self, x1, y1, x2, y2, s):
+        self.head = [PlugHead(x1, y1, 40, s[0]), PlugHead(x2, y2, 40, s[1])]
+        self.color = "purple"
+
+    def update(self, pos):
+        for h in self.head:
+            if h.picked:
+                h.update(pos)
+
+    def pick(self, pos):
+        for h in self.head:
+            if h.rect.collidepoint(pos):
+                h.pick(pos)
+
+    def setPlug(self):
+        for h in self.head:
+            if h.picked == True:
+                h.setPlug()
+
+    def render(self):
+        pygame.draw.rect(screen, self.color, self.head[0].rect,  border_radius = 15)
+        pygame.draw.rect(screen, self.color, self.head[1].rect, border_radius = 15)
+        pygame.draw.line(screen, self.color, self.head[0].rect.center, self.head[1].rect.center, width = 5)
+
+
+def alpha2index(a):
+    return ord(a) - ord('A') #TODO this doesn't feel safe. what if the aplhas are not contiguous
+
+def index2alpha(n):
+    return chr(ord('A') + n) #TODO this doesn't feel safe. what if the aplhas are not contiguous
 
 def render():
-    for key in keys.values():
-        key.render()
+    if state == LIGHTBOARD:
+        for key in keys.values():
+            key.render()
 
-    sizeFont = fontRotor.size(" 00 ")
-    space = 10
-    x = (width/2) + ((sizeFont[0]/2)+space)
-    y = 25
-    for r in rotor[1:-1]:
-        r.render(x, y)
-        x -= sizeFont[0]+space
+        sizeFont = fontRotor.size(" 00 ")
+        space = 10
+        x = (width/2) + ((sizeFont[0]/2)+space)
+        y = 25
+        for r in rotor[1:-1]:
+            r.render(x, y)
+            x -= sizeFont[0]+space
+
+    if state == PLUGBOARD:
+        for plug in sockets.values():
+            plug.render()
+        for plug in plugs:
+            plug.render()
 
 def init():
+    r1 = ['J', 'G', 'D', 'Q', 'O', 'X', 'U', 'S', 'C', 'A', 'M', 'I', 'F', 'R', 'V', 'T', 'P', 'N', 'E', 'W', 'K', 'B', 'L', 'Z', 'Y', 'H']
+    r2 = ['N', 'T', 'Z', 'P', 'S', 'F', 'B', 'O', 'K', 'M', 'W', 'R', 'C', 'J', 'D', 'I', 'V', 'L', 'A', 'E', 'Y', 'U', 'X', 'H', 'G', 'Q']
+    r3 = ['J', 'V', 'I', 'U', 'B', 'H', 'T', 'C', 'D', 'Y', 'A', 'K', 'E', 'Q', 'Z', 'P', 'O', 'S', 'G', 'X', 'N', 'R', 'M', 'W', 'F', 'L']
+    ukw = ['Q', 'Y', 'H', 'O', 'G', 'N', 'E', 'C', 'V', 'P', 'U', 'Z', 'T', 'F', 'D', 'J', 'A', 'X', 'W', 'M', 'K', 'I', 'S', 'R', 'B', 'L']
+    plugboard = ['A', 'B', 'K', 'D', 'R', 'U', 'G', 'T', 'I', 'J', 'C', 'L', 'O', 'N', 'M', 'P', 'Q', 'E', 'S', 'H', 'F', 'V', 'W', 'X', 'Y', 'Z']
+
     def createKeys(x, y, string):
         for s in string:
             keys[s] = KeyLight(x, y, s)
+            sockets[s] = Socket(x, y, s)
             x += 100
 
     createKeys(100, 100, "QWERTYUIOP")
     createKeys(150, 200, "ASDFGHJKL")
     createKeys(250, 300, "ZXCVBNM")
 
-    r1 = ['J', 'G', 'D', 'Q', 'O', 'X', 'U', 'S', 'C', 'A', 'M', 'I', 'F', 'R', 'V', 'T', 'P', 'N', 'E', 'W', 'K', 'B', 'L', 'Z', 'Y', 'H']
-    r2 = ['N', 'T', 'Z', 'P', 'S', 'F', 'B', 'O', 'K', 'M', 'W', 'R', 'C', 'J', 'D', 'I', 'V', 'L', 'A', 'E', 'Y', 'U', 'X', 'H', 'G', 'Q']
-    r3 = ['J', 'V', 'I', 'U', 'B', 'H', 'T', 'C', 'D', 'Y', 'A', 'K', 'E', 'Q', 'Z', 'P', 'O', 'S', 'G', 'X', 'N', 'R', 'M', 'W', 'F', 'L']
-    ukw = ['Q', 'Y', 'H', 'O', 'G', 'N', 'E', 'C', 'V', 'P', 'U', 'Z', 'T', 'F', 'D', 'J', 'A', 'X', 'W', 'M', 'K', 'I', 'S', 'R', 'B', 'L']
-    plugboard = ['B', 'A', 'D', 'C', 'F', 'E', 'H', 'G', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    for n in range(26):
+        for p in plugs:
+            if chr(ord('A')+n) in (p.head[0].s, p.head[1].s):
+                break
+        else:
+            if chr(ord('A')+n) != plugboard[n]:
+                plugs.append(Plug(sockets[chr(ord('A')+n)].x,
+                                            sockets[chr(ord('A')+n)].y,
+                                            sockets[plugboard[n]].x,
+                                            sockets[plugboard[n]].y,
+                                            [chr(ord('A')+n), plugboard[n]]))
 
     def addRotor(r):
         if len(rotor) == 0:
@@ -98,12 +206,21 @@ def init():
     addRotor(Rotor(r3))
     addRotor(Rotor(ukw))
 
-
+PLUGBOARD = 1
+LIGHTBOARD = 2
+state = LIGHTBOARD
 pygame.init()
 font = pygame.font.Font(None, 64)
 fontRotor = pygame.font.Font(None, 32)
 height = 400
 width = 1100
+keys = {}
+keyPressed = None
+keyOut = None
+picked = False
+rotor = []
+plugs = []
+sockets = {}
 screen = pygame.display.set_mode([width, height])
 init()
 
@@ -114,17 +231,43 @@ while not quit:
             quit = True
 
         if e.type == pygame.KEYDOWN:
-            if keyPressed is None and e.unicode.upper() in keys:
-                keyPressed = e.unicode
-                keyOut = rotor[0].output(keyPressed.upper())
-                rotor[1].inc()
-                keys[keyOut].lit = True
-                print(keyOut, end="")
+            if e.key == pygame.K_SPACE:
+                if state == LIGHTBOARD:
+                    state = PLUGBOARD
+                elif state == PLUGBOARD:
+                    state = LIGHTBOARD
+
+            if state == LIGHTBOARD:
+                if keyPressed is None and e.unicode.upper() in keys:
+                    keyPressed = e.unicode
+                    keyOut = rotor[0].output(keyPressed.upper())
+                    rotor[1].inc() #Can I make this implicit in the class inself may be a class enigma that wraps, rotors and plugboard and light
+                    keys[keyOut].lit = True
+                    print(keyOut, end="")
 
         if e.type == pygame.KEYUP:
-            if keyPressed is e.unicode:
-                keyPressed = None
-                keys[keyOut].lit = False
+            if state == LIGHTBOARD:
+                if keyPressed is e.unicode:
+                    keyPressed = None
+                    keys[keyOut].lit = False
+
+        if e.type == pygame.MOUSEBUTTONDOWN:
+            if state == PLUGBOARD:
+                if picked:
+                    for p in plugs:
+                        p.setPlug()
+                else:
+                    pos = pygame.mouse.get_pos()
+                    for p in plugs:
+                        p.pick(pos)
+
+            if state == LIGHTBOARD:
+                for r in rotor[1:-1]:
+                    r.set_rotor(pygame.mouse.get_pos())
+
+        if state == PLUGBOARD:
+            for p in plugs:
+                p.update(pygame.mouse.get_pos())
 
     screen.fill((255, 255, 255))
     render()
